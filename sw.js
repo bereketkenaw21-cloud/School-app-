4fd4a40dd334a0560ff011a38be5471fd60f6337
bereketkenaw21-cloud <bereketkenaw21@gmail.com>
2026-03-08 16:55:24 +0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'mertule-school-v2';
+const assets = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json'
+];
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+});
